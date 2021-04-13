5309c0e6b76d51366d889c9326439682fe90dfa9
Michal Horvath <mhsfdev@gmail.com>
2021-04-13 19:49:40 +0200

    simple loop with move and check of own piece or

--- main.py
+++ main.py
@@ -37,18 +37,39 @@ p_w = Pawn('w')
 
 player_A = [[p_b,'a1'],[p_b,'b1'],[p_b,'c1']]
 player_B = [[p_w,'a3'],[p_w,'b3'],[p_w,'c3']]
-
+players = [player_A,player_B]
 
 for piece, position in player_A:
     b.place_item(piece, position)
 
 for piece, position in player_B:
     b.place_item(piece, position)
-
+turn = 1
 b.visualize()
+while True:
+    playing_player = turn%2
+    
+    try: # taking move to do
+        from_position, to_position =  input(f'whats next move {players[playing_player]}? ').split(',')
+    except ValueError:
+        break
+    
+    if b.is_free(to_position): # if destination is free, move i done, turn is completed
+        b.move(from_position, to_position)
+        turn +=1
+    else:
+        piece_out = b.get_item(to_position) # if destination is taken by friendly piece
+        if b.get_item(from_position).color == piece_out.color:
+            print('you fool, there is your piece!! Again')
+            continue # turn is not completed 
 
-print(b.move('b1','c2'))
-b.visualize()
+        b.move(from_position, to_position)
+
+    b.visualize()
+    print(player_A)
+    print(player_B)
+
+
 
             
 
