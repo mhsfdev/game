b4f763794968d7262bef08aaa7aa5f1b442d9740
Michal Horváth <>
2021-04-14 06:38:43 +0000

    seting players piece set as dictionary with positions as keys

--- main.py
+++ main.py
@@ -29,17 +29,27 @@ class Player:
 
     def __init__(self, name, color):
         self.name = name
-        self.pieces = []
+        self.pieces = {} 
+        """ enables to work with dictionary methods to retrieve Piece into gaming
+        """
         if color not in ['b','w']:
             raise ValueError(f'Color {color} is not valid')
+        else:
+            self.color = color
     
-    def set_pieces(self, positions):
-        for position in positions:
-            self.pieces.append( position)
+    def __repr__(self):
+        return self.name
     
-    def remove_piece(self, position): # find record with position and remove it from the list
-        pass
+    def set_piece(self, positions, piece):
+        try:
+            piece.color  = self.color
+        except ValueError:
+            raise ValueError(f'Piece {piece} has no color attribute')
 
+        for position in positions:
+            self.pieces[position]= piece
+    
+    
 
 
 
@@ -52,28 +62,37 @@ b=Board(size)
 p_b = Pawn('b')
 p_w = Pawn('w')
 
-player_A = [[p_b,'a1'],[p_b,'b1'],[p_b,'c1']]
-player_B = [[p_w,'a3'],[p_w,'b3'],[p_w,'c3']]
+player_A = Player('Hugo','b')
+player_B = Player('Elvis','w')
+
+player_A.set_piece(['a1','b1','c1'], p_b)
+
+player_B.set_piece(['a3','b3','c3'], p_w)
 players = [player_A,player_B]
 
-for piece, position in player_A:
+for position,piece in player_A.pieces.items():
     b.place_item(piece, position)
 
-for piece, position in player_B:
+for position,piece in player_B.pieces.items():
     b.place_item(piece, position)
+
+
 turn = 1
 b.visualize()
 while True:
     playing_player = turn%2
     
     try: # taking move to do
-        from_position, to_position =  input(f'whats next move {players[playing_player]}? ').split(',')
+        from_position, to_position =  input(f'whats your next move {players[playing_player]}? ').split(',')
     except ValueError:
         break
+    from_position, to_position = from_position.strip(), to_position.strip() # stripped of unneccessary blanks
+    
     
     if b.is_free(to_position): # if destination is free, move i done, turn is completed
         b.move(from_position, to_position)
         turn +=1
+        # make changes in players sets
     else:
         piece_out = b.get_item(to_position) # if destination is taken by friendly piece
         if b.get_item(from_position).color == piece_out.color:
