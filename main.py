a575704aea143afbca1972ab82b8a7b669aa0f41
Michal Horváth <82112622+mhsfdev@users.noreply.github.com>
2021-08-29 09:54:31 +0000

    correction of main.py and board

--- main.py
+++ main.py
@@ -9,19 +9,19 @@ b = Board(size)
 p_b = Pawn('b')
 p_w = Pawn('w')
 
-player_A = Player('Hugo', 'b')
-player_B = Player('Elvis', 'w')
+player_A = Player('Hugo', color = 'b')
+player_B = Player('Elvis', color = 'w')
 
 player_A.set_piece(['a3', 'b3', 'c3'], p_b)
 
-player_B.set_piece(['a2', 'b2', 'c2'], p_w)
+player_B.set_piece(['a1', 'b1', 'c1'], p_w)
 players = [player_A, player_B]
 
 for position, piece in player_A.pieces.items():
-    b.place_item(piece, position)
+    b[position] = piece
 
 for position, piece in player_B.pieces.items():
-    b.place_item(piece, position)
+    b[position] = piece
 
 turn = 1
 b.show()
@@ -46,7 +46,7 @@ while len(b.has_legal_moves(*players[turn%2].pieces.keys())) != 0: #winning cond
         print(f'You do not have piece on position {from_position}')
         continue
 
-    moving_piece = b.get_item(from_position)
+    moving_piece = b[from_position]
 
     move = b.move(from_position, to_position)
 
@@ -68,5 +68,5 @@ while len(b.has_legal_moves(*players[turn%2].pieces.keys())) != 0: #winning cond
 
         print('not valid move , buddy')
 
-print (f'game over for player {players[players[turn%2]} he has not valid moves to make !!!' )
+print (f'game over for player {players[players[turn%2]]} he has not valid moves to make !!!' )
 
